9dca2ebe357652d404892a53e135b2ef01d5fb4f
Juliano Seus <juliano.seus@hotmail.com>
2020-11-18 20:55:46 -0300

    Criacao da classe SQL e do Banco de Dados

--- src/index.php
+++ src/index.php
@@ -3,3 +3,23 @@
 /*
 * É recomendado que todo o carregamente seja feito apartir desse arquivo.
 */
+    include_once("model/Sql.php");
+
+    $dado = new Sql();
+
+    $agora = SUNFUNCS_RET_TIMESTAMP;
+    
+    /*$dado->query("INSERT INTO medico (email, nome, senha) VALUES (:EMAIL, :NOME, :SENHA)", 
+    array(
+      ":EMAIL" => "a@a.com",  
+      ":NOME" => "alex",
+      ":SENHA" => "senha1"
+    ));
+    */
+
+    $results = $dado->select("SELECT * FROM medico ORDER BY id");
+
+    echo json_encode($results);
+
+    
+?>
